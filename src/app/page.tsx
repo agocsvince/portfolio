724a1538eee07e5fb6c8ed885e25f41b0e5b065c
Vince Agocs <vince.agocs@gmail.com>
2025-04-05 16:43:40 +0200

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+export default function Home() {
+
+
+
+  return (
+    <div className="">
+      <main className="flex flex-col">
+        {/* MAIN */}
+          <div className="flex-2/3 bg-secondary h-full w-full">
+            <p>LOREM IPSUM</p>
+          </div>
+          <div className="flex-1/3">
+            <p>LOREM IPSUM</p>
+          </div>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        {/* FOOTER */}
+      </footer>
+    </div>
+  );
+}
