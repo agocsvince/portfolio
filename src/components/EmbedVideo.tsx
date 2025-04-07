6711065bc32a2d23ea60393932d79a826bfe9ee4
Vince Agocs <vince.agocs@gmail.com>
2025-04-07 22:53:12 +0200

    fix: update video component structure to correctly pass video data and enhance loading state handling

--- src/components/EmbedVideo.tsx
+++ src/components/EmbedVideo.tsx
@@ -1,17 +1,27 @@
-import { assetType } from '@/helpers/types'
+import { videoType } from '@/helpers/types'
 import React, { useState } from 'react'
 import VideoLoadPoster from './VideoLoadPoster'
 
-const EmbedVideo = ({ video }: { video: assetType }) => {
+const EmbedVideo = ({ video }: { video: videoType }) => {
   const [isLoading, setIsLoading] = useState(true)
+  const [showInfo, setShowInfo] = useState(false);
+
 
   return (
-    <div className='relative h-full w-full aspect-video'>
+    <div className='relative h-full w-full aspect-video'  onClick={() => setShowInfo(!showInfo)}>
         <video onCanPlayThrough={() => setIsLoading(false)} autoPlay muted loop playsInline preload='metadata'>
-          <source src={video.url} type={video.mimeType} />
+          <source src={video.asset.url} type={video.asset.mimeType} />
 
         </video>
         {isLoading && <VideoLoadPoster />}
+        <div className={`${showInfo ? `opacity-100` : `opacity-0`} p-4 sm:p-8 justify-between flex flex-col transition-opacity ease-in-out h-full w-full bg-dark-primary/80 absolute top-0 left-0`}>
+          <div className='flex flex-col'>
+            <h2 className='text-xs sm:text-base opacity-80'>{video.type}</h2>
+            <h1 className='text-base sm:text-2xl'>{video.title}</h1>
+            <p>{video.description}</p>
+          </div>
+          <h2 className='text-xs sm:text-base opacity-80'>{video.date}</h2>
+        </div>
     </div>
   )
 }
--- src/components/VideoReel.tsx
+++ src/components/VideoReel.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 import EmbedVideo from './EmbedVideo';
 
 const VideoReel = ({ videos}: { videos: videoType[]}) => {
-  return ( <>
+
+  return (<>
     {videos.map((video, index) => {
       return (
-        <div key={video.id} className='flex flex-col gap-8'>
-          <EmbedVideo video={video.asset} />
+        <div key={video.id} className='relative flex flex-col gap-8'>
+          <EmbedVideo video={video} />
           {index !== videos.length - 1 && <hr />}
+          
         </div>
       );
     })}
