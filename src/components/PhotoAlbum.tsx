8846e9e4db41bf8357c14d55185af99c22de23cf
Vince Agocs <vince.agocs@gmail.com>
2025-04-15 17:02:04 +0200

    feat: add photo album slider styles for navigation arrows

--- src/components/PhotoAlbum.tsx
+++ src/components/PhotoAlbum.tsx
@@ -2,30 +2,30 @@ import { photoType } from '@/helpers/types';
 import Image from 'next/image';
 import React from 'react'
 
+import 'react-photo-view/dist/react-photo-view.css';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+
+
 const PhotoAlbum = ({ photos }: { photos: photoType[]}) => {
-  const onImageClick = () => {
-
-    
-  }
-
   return (
     <div className='flex flex-row flex-wrap p-4 gap-8 justify-center bg-light-primary'>
-      {photos.map((photo) => {
-        return (
-          <div
-            key={photo.id}
-            className={`${photo.width > photo.height ? 'flex-[35%]' : 'flex-[25%]'} flex flex-col gap-4`}
-            onClick={() => onImageClick()}
-          >
-            <Image
-              src={photo.url}
-              alt={`Photo of ${photo.fileName}`}
-              width={photo.width}
-              height={photo.height}
-            />
-          </div>
-        );
-      })}
+      <PhotoProvider>
+        {photos.map((photo) => (
+            <div
+              key={photo.id}
+              className={`${photo.width > photo.height ? 'flex-[35%]' : 'flex-[25%]'} flex flex-col gap-4`}
+            >
+              <PhotoView src={photo.url}>
+                <Image
+                  src={photo.url}
+                  alt={`Photo of ${photo.fileName}`}
+                  width={photo.width}
+                  height={photo.height}
+                />
+              </PhotoView>
+            </div>
+          ))}
+      </PhotoProvider>
     </div>
   )
 }
