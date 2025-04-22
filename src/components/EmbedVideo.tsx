912186a373b6eed71a35840bb203ea6f70a44633
Vince Agocs <vince.agocs@gmail.com>
2025-04-22 18:32:16 +0200

    fix: improve layout of video info section in EmbedVideo component

--- src/components/EmbedVideo.tsx
+++ src/components/EmbedVideo.tsx
@@ -15,14 +15,16 @@ const EmbedVideo = ({ video, defaultInfoState = false }: { video: videoType, def
         </video>
         {isLoading && <Loader />}
         <div className={`${showInfo ? `opacity-100 scale-100` : `opacity-0 scale-0`} p-4 sm:p-8 justify-between flex flex-col transition-opacity ease-in-out h-[calc(100%+1px)] w-full bg-dark-primary/80 absolute top-0 left-0`}>
-        <div className='flex flex-row justify-between'>
-          <div className='flex flex-col'>
+        <div className='flex flex-row justify-between gap-5'>
+          <div className='flex flex-col max-w-[85%]'>
             <h2 className='text-xs sm:text-base opacity-80'>{video.type}</h2>
             <h1 className='text-base sm:text-2xl'>{video.title}</h1>
-            <p>{video.description}</p>
           </div>
-            <a className='text-xs sm:text-base opacity-80 hover:opacity-50 underline' href={video.url}>See original</a>
+          <div>
+            <a className='text-xs sm:text-base opacity-80 hover:opacity-50 underline cursor-pointer' href={video.url}>See original</a>
+          </div>
         </div>
+          <p className='text-xs sm:text-sm md:text-base overflow-hidden line-clamp-5 sm:line-clamp-8'>{video.description}</p>
           <h2 className='text-xs sm:text-base opacity-80'>{formatDate(video.date)}</h2>
         </div>
     </div>
