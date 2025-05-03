dc60e58d77c986de45bbd78e715ffd25b6a91c25
Vince Agocs <vince.agocs@gmail.com>
2025-05-03 22:29:46 +0200

    fix: adjust styling in TechStackInfo component for improved readability

--- src/components/TechStackInfo.tsx
+++ src/components/TechStackInfo.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const TechStackInfo = ({data}: {data: string[]}) => {
   return (
-    <div className='rounded-[60px] z-3 bg-dark-primary/70 absolute top-0 left-0 h-full w-full flex felx-col items-center justify-center'>
+    <div className='rounded-[45px] z-3 bg-dark-primary/70 absolute top-0 left-0 h-full w-full flex felx-col items-center justify-center'>
       <ul className='text-center'>
         {data.map((lang, index) => {
-          return <li className='py-2' key={index}>{lang}</li>
+          return <li className='py-2 text-xl' key={index}>{lang}</li>
         })}
       </ul>
     </div>
