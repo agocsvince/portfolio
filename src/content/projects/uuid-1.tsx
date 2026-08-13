9b84b03c67917aabc410bc23b04638497a863783
Vince Agocs <vince.agocs@gmail.com>
2026-08-13 15:45:54 +0200

    Add animation

--- src/content/projects/uuid-1.tsx
+++ src/content/projects/uuid-1.tsx
@@ -6,7 +6,7 @@ export const content = (
   <div>
     <h1 className="text-2xl mb-4">README.md</h1>
     <div className="flex flex-col-reverse md:flex-row gap-4">
-      <div>
+      <div className="flex flex-col gap-8">
         <p>
           {' '}
           I'm a full-stack developer with a strong focus on front-end
@@ -16,23 +16,20 @@ export const content = (
           projects—from large-scale platforms to lean startup environments—often
           taking ownership of the entire development process.
         </p>
-        <br />
         <p>
           {' '}
           In addition to coding, I have a deep passion for visual storytelling.
           I shoot and edit music videos, short films, and other creative video
           content, and I'm also experienced in photography.
         </p>
-        <br />
         <p>
           I enjoy bringing ideas to life—whether it's through code, camera, or a
           blend of both.
         </p>
-        <br />
-        <div>
+        <div className="flex flex-col gap-2">
           <span>feel free to contact me at: {'  '}</span>
           <a
-            className="bg-primary text-alternative outline-1 outline-alternative px-2 py-1 hover:opacity-90 active:opacity-80 transition-opacity"
+            className="shine relative w-min bg-primary text-alternative px-2 py-1 hover:opacity-90 active:opacity-80 transition-opacity"
             href="mailto:contact@agocsvince.com"
           >
             contact@agocsvince.com
